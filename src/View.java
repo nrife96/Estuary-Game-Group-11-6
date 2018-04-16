c01f10d6b0151ccb0e4d7d8ee06f704f35db85c7
Nick Rife <nrife@udel.edu>
2018-04-16 17:41:51 -0400

    organized thangs

---
+++ src/View.java
@@ -0,0 +1,11 @@
+import java.awt.image.BufferedImage;
+
+class View {
+	BufferedImage[] boat;
+	BufferedImage[] wake;
+	BufferedImage[] sand;
+	BufferedImage[][] barriers;
+	BufferedImage water;		// assuming single (background) image of water
+
+	
+}
