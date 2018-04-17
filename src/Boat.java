1f034183f4089b03cde451dec94726b68aae02e5
cocotero <mferrato@github.com>
2018-04-17 11:40:16 -0400

    Created Shore and Boat class stubs

---
+++ src/Boat.java
@@ -0,0 +1,10 @@
+class Boat extends Moveable{
+
+	private int speed;
+	private String direction;
+
+	public void updateLoc(){
+		//implemented later
+	}
+
+}
---
+++ src/Shore.java
@@ -0,0 +1,8 @@
+class Shore extends Stationary{
+
+	@Override
+	public void create(){
+		//implemented later
+	}
+
+}
