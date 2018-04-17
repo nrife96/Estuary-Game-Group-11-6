40d154e7745c6fabe707f613aeb3a75a3807c339
kburd <kburd@udel.edu>
2018-04-16 20:09:41 -0400

    Adding Item stub

---
+++ Item.java
@@ -0,0 +1,16 @@
+
+class Item{
+
+    int xLoc;
+    int yLoc;
+    int width;
+    int height;
+
+    public Item(int xLoc, int yLoc, int width, int height){
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+        this.width = width;
+        this.height = height;
+    }
+
+}
