2d58a46c4287cc01587937839d94cdba66d0f4a2
kburd <kburd@udel.edu>
2018-04-18 10:36:55 -0400

    implemented controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,17 @@
-public class Controller {
+
+import java.awt.EventQueue;
+import javax.swing.Timer;
+import javax.swing.JFrame;
+import javax.swing.AbstractAction;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+
+public class Controller extends JFrame {
+
+    Action drawAction;
+    final int drawDelay = 30; //msec
+
 	private Model model;
 	private View view;
 	
@@ -6,13 +19,35 @@ public class Controller {
 	
 	int timeRemaining; //
 	
-	public Controller() {
-		//update
+	public Controller(){
+		drawAction = new AbstractAction(){
+            public void actionPerformed(ActionEvent e){
+                // update based on whether the game is "paused"
+
+                model.update();
+                view.update();
+
+            }//actionPerformed
+		};//AbastractAction
+		
+		add(view);
+        
+        // add the settings
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());                // i added this (helps display the button correctly)
+        setSize(view.getWidth(), view.getHeight());
+        setVisible(true);
+
 	}
-	
-	
-	public void start() {
-		//update
-	}
+
+	public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                Controller a = new Controller();
+                Timer t = new Timer(a.drawDelay, a.drawAction);
+                t.start();
+            }//run
+        });//runnable
+    }//main
 
 }
