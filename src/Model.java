2469f1fe10a30f0da743ece08128f885f0e007fb
kburd <kburd@udel.edu>
2018-04-18 19:19:31 -0400

    added spawnBoat and fixed a bug in Tool

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,6 @@
 import java.util.Collection;
 import java.util.ArrayList;
+import java.util.Random;
 
 class Model{
     
@@ -13,20 +14,74 @@ class Model{
     Tool tool;
     int level;              //difficulty level
     final int maxBoats = 0; //temporary value, this will hold the maximum number of boats on screen at a given time
-    
+    final int maxSpeed = 1;
+    final int numOfLanes = 5;
+
+    Random rand = new Random();
+
+    int frameHeight;
+    int frameWidth;
+
+
+
+
     public Model(){
 
         fleet = new ArrayList<Boat>();
         wakes = new ArrayList<Wake>();
         shoreline = new ArrayList<Shore>();
         barrierDefense = new ArrayList<Barrier>();
+
+        frameHeight = 100;
+        frameWidth = 100;
         
     }//Model
+
+    public void update(){
+        spawnBoat();
+    }//update
         
 
     
-    public void addBoat(Boat boat){
-        fleet.add(boat);
+    public void spawnBoat(){
+
+        int spawnChance = 100; //Spawns every n times
+
+        if(rand.nextInt(spawnChance) == 0){
+            
+            //Assuming origin is bottom left
+
+            //Pick left(0) or right(1) side
+            int dirInt = rand.nextInt(2);
+            String direction;
+            if(dirInt == 1){
+                direction = "Left";
+            }
+            else{
+                direction = "Right";
+            }
+
+            //Pick side of screen to start on
+            int xLoc = dirInt*frameWidth;
+
+            //Pick height to start at
+            int waterSpace = (frameHeight/2);
+            int laneHeight = waterSpace/numOfLanes;
+            int laneNum = rand.nextInt(numOfLanes);
+            int yLoc = laneNum*laneHeight + waterSpace;
+            
+            //Random speed up to max
+            int speed = rand.nextInt(maxSpeed+1);
+
+            System.out.println(xLoc + " " + yLoc + " " + speed + " " + direction);
+
+            Boat newBoat = new Boat(xLoc,yLoc,1,1,speed,direction);
+
+        }
+
+
+
+        // fleet.add(boat);
         
     }//addBoat
     
@@ -34,18 +89,13 @@ class Model{
         shore.destroy();
     }//removeShore
     
-    
-    public void update(){
-        
-    }//update
-    
     public void checkForCollisions(){
         
     }//checkForCollisions
     
     public void switchTool(Tool newTool){
         
-    }//switchTool
+    }//switchTool1
     
     
     
--- src/Tool.java
+++ src/Tool.java
@@ -4,6 +4,13 @@ public enum Tool {
     SHOVEL("shovel",1);
 
     private static Tool[] tools = values();
+    String name;
+    int index;
+
+    private Tool(String s, int i){
+		name = s;
+		index = i;
+	}
 
     public Tool next(){
         return tools[(this.ordinal()+1) % tools.length];
