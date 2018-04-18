3a37fdafa0cdfd08cecc5983c4db68cf1858ca49
kburd <kburd@udel.edu>
2018-04-18 10:52:59 -0400

    added getters and setter to barrier

--- src/Barrier.java
+++ src/Barrier.java
@@ -2,10 +2,28 @@
 class Barrier extends Stationary{
 
     String type;
+    int health;
 
-	public Barrier(int xLoc, int yLoc, int height, int width, String type) {
+	public Barrier(int xLoc, int yLoc, int height, int width, String type, int health) {
         super(xLoc, yLoc, height, width);
         this.type = type;
-	}
+        this.health = health;
+    }
+    
+    public int getHealth(){
+        return health;
+    }
+
+    public int setHealth(int newHealth){
+        this.health = newHealth;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public int setType(int type){
+        this.type = type;
+    }
 
 }
