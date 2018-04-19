b05f6be785daefbd406e35cf93c587ddc6aa353a
Jason Reynolds <jkr@udel.edu>
2018-04-19 14:46:29 -0400

    added direction to the wakes

--- src/Wake.java
+++ src/Wake.java
@@ -3,7 +3,7 @@ class Wake extends Moveable{
     
     String direction;
     
-    public Wake(int xLoc, int yLoc, int height, int width, int speed){
+    public Wake(int xLoc, int yLoc, int height, int width, int speed,String direction){
 
         //Moves 0 in the x, only in the y
         super(xLoc, yLoc, height, width, 0, speed);
